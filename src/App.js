abaaa832addf3ccbb5f2cd132583117a30bc066e
Omina <omina.mamatalieva.ch@gmail.com>
2022-10-16 16:10:36 +0600

    added if else

--- src/App.js
+++ src/App.js
@@ -1,7 +1,54 @@
 import "./App.css";
 
+
+const blocks = [
+  {
+    id: 'b1',
+    block: 'yellow',
+  },
+  {
+    id: 'b2',
+    block: 'green',
+  },
+  {
+    id: 'b3',
+    block: 'color',
+  },
+  {
+    id: 'b4',
+    block: `${Math.random()}`,
+  },
+] 
+
+
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      {blocks.map((el)=>{
+        let color=el.block;
+
+        if(color=="yellow"){
+          return(
+            <div className="b1" key={el.id}></div>
+          )
+        } else if(color=="green"){
+          return(
+            <div className="b2" key={el.id}></div>
+          )
+        }else if(color==="color"){
+          return(
+            <div className="b3" key={el.id}></div>
+          )
+        }else if(color=Number.isFinite(0)){
+          return(
+            <div className="b4" key={el.id}></div>
+          )
+        }
+      })}
+   
+   </div>
+  );
 }
 
 export default App;
+
